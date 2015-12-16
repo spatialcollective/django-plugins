6a218033d285cdcb16f8bfbe3c8d8fcd2d411908
spatialcollective <jamie@spatialcollective.com>
2015-12-16 10:28:28 +0300

    Update __init__.py

--- djangoplugins/management/__init__.py
+++ djangoplugins/management/__init__.py
@@ -5,9 +5,6 @@ try:
 except ImportError:
     from south.signals import post_migrate
 
-from django.db.models.signals import post_syncdb
-
-
 from djangoplugins import models as plugins_app
 from .commands.syncplugins import SyncPlugins
 
@@ -17,7 +14,3 @@ def sync_plugins(sender, verbosity, **kwargs):
 
 # Always run after `migrate`.
 post_migrate.connect(sync_plugins, sender=plugins_app)
-
-# This is somehow needed for the tests to work.
-# XXX: Figure out how to get rid of this.
-post_syncdb.connect(sync_plugins, sender=plugins_app)
